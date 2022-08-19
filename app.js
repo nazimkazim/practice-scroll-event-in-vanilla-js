0e3b79823c0427c6dacdb75712aa3b48e46375b8
etun <etun@technodom.kz>
2022-08-19 09:20:30 +0600

    add mouseout, mousein, mouseover events

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const box1 = document.querySelector('.box1')
+const box2 = document.querySelector('.box2')
+const box3 = document.querySelector('.box3')
+
+
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY
@@ -11,4 +15,41 @@ window.addEventListener('scroll', () => {
         box1.classList.remove('fading')
         box1.classList.add('unfading')
     }
+})
+
+
+let dbcl = false
+
+
+box2.addEventListener('dblclick', (e) => {
+    dbcl = !dbcl
+    if (dbcl) {
+        e.target.textContent = 'Wow'
+        e.target.style.backgroundColor = 'aqua'
+        e.target.style.color = 'rebeccapurple'
+        e.target.style.border = '1px dotted rebeccapurple'
+    } else {
+        e.target.textContent = 'box2'
+        e.target.style.backgroundColor = 'rebeccapurple'
+        e.target.style.color = 'white'
+    }
+
+
+})
+
+box3.addEventListener('mouseover', (e) => {
+    e.target.textContent = 'mouse in'
+    e.target.style.backgroundColor = 'aqua'
+    e.target.style.color = 'rebeccapurple'
+    e.target.style.border = '1px dotted rebeccapurple'
+})
+
+box3.addEventListener('mouseout', (e) => {
+    e.target.textContent = 'mouse out'
+    e.target.style.backgroundColor = 'rebeccapurple'
+    e.target.style.color = 'white'
+})
+
+box3.addEventListener('mousemove', (e) => {
+    e.target.style.backgroundColor = `rgb(${0.25 * e.clientX},${1.4 * e.clientY},${0.3 * e.clientX})`
 })
